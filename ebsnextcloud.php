8359c3b44b556461fa919cafda83ffb3690acb65
alexandre <alexandre@EBS-LP-ALEXANDRE.EBS.local>
2019-01-28 14:05:02 +1100

    creation OK

--- ebsnextcloud.php
+++ ebsnextcloud.php
@@ -451,33 +451,33 @@ function ebsnextcloud_TestConnection(array $params)
 //    }
 //}
 
-function ebsnextcloud_ServiceSingleSignOn(array $params)
-{
-    try {
-        // Call the service's single sign-on token retrieval function, using the
-        // values provided by WHMCS in `$params`.
-        $response = array();
-
-        return array(
-            'success' => true,
-            'redirectTo' => $response['redirectUrl'],
-        );
-    } catch (Exception $e) {
-        // Record the error in WHMCS's module log.
-        logModuleCall(
-            'provisioningmodule',
-            __FUNCTION__,
-            $params,
-            $e->getMessage(),
-            $e->getTraceAsString()
-        );
-
-        return array(
-            'success' => false,
-            'errorMsg' => $e->getMessage(),
-        );
-    }
-}
+//function ebsnextcloud_ServiceSingleSignOn(array $params)
+//{
+//    try {
+//        // Call the service's single sign-on token retrieval function, using the
+//        // values provided by WHMCS in `$params`.
+//        $response = array();
+//
+//        return array(
+//            'success' => true,
+//            'redirectTo' => $response['redirectUrl'],
+//        );
+//    } catch (Exception $e) {
+//        // Record the error in WHMCS's module log.
+//        logModuleCall(
+//            'provisioningmodule',
+//            __FUNCTION__,
+//            $params,
+//            $e->getMessage(),
+//            $e->getTraceAsString()
+//        );
+//
+//        return array(
+//            'success' => false,
+//            'errorMsg' => $e->getMessage(),
+//        );
+//    }
+//}
 
 function ebsnextcloud_AdminSingleSignOn(array $params)
 {
@@ -507,51 +507,51 @@ function ebsnextcloud_AdminSingleSignOn(array $params)
     }
 }
 
-function ebsnextcloud_ClientArea(array $params)
-{
-    // Determine the requested action and set service call parameters based on
-    // the action.
-    $requestedAction = isset($_REQUEST['customAction']) ? $_REQUEST['customAction'] : '';
-
-    if ($requestedAction == 'manage') {
-        $serviceAction = 'get_usage';
-        $templateFile = 'templates/manage.tpl';
-    } else {
-        $serviceAction = 'get_stats';
-        $templateFile = 'templates/overview.tpl';
-    }
-
-    try {
-        // Call the service's function based on the request action, using the
-        // values provided by WHMCS in `$params`.
-        $response = array();
-
-        $extraVariable1 = 'abc';
-        $extraVariable2 = '123';
-
-        return array(
-            'tabOverviewReplacementTemplate' => $templateFile,
-            'templateVariables' => array(
-                'extraVariable1' => $extraVariable1,
-                'extraVariable2' => $extraVariable2,
-            ),
-        );
-    } catch (Exception $e) {
-        // Record the error in WHMCS's module log.
-        logModuleCall(
-            'provisioningmodule',
-            __FUNCTION__,
-            $params,
-            $e->getMessage(),
-            $e->getTraceAsString()
-        );
-
-        // In an error condition, display an error page.
-        return array(
-            'tabOverviewReplacementTemplate' => 'error.tpl',
-            'templateVariables' => array(
-                'usefulErrorHelper' => $e->getMessage(),
-            ),
-        );
-    }
-}
+//function ebsnextcloud_ClientArea(array $params)
+//{
+//    // Determine the requested action and set service call parameters based on
+//    // the action.
+//    $requestedAction = isset($_REQUEST['customAction']) ? $_REQUEST['customAction'] : '';
+//
+//    if ($requestedAction == 'manage') {
+//        $serviceAction = 'get_usage';
+//        $templateFile = 'templates/manage.tpl';
+//    } else {
+//        $serviceAction = 'get_stats';
+//        $templateFile = 'templates/overview.tpl';
+//    }
+//
+//    try {
+//        // Call the service's function based on the request action, using the
+//        // values provided by WHMCS in `$params`.
+//        $response = array();
+//
+//        $extraVariable1 = 'abc';
+//        $extraVariable2 = '123';
+//
+//        return array(
+//            'tabOverviewReplacementTemplate' => $templateFile,
+//            'templateVariables' => array(
+//                'extraVariable1' => $extraVariable1,
+//                'extraVariable2' => $extraVariable2,
+//            ),
+//        );
+//    } catch (Exception $e) {
+//        // Record the error in WHMCS's module log.
+//        logModuleCall(
+//            'provisioningmodule',
+//            __FUNCTION__,
+//            $params,
+//            $e->getMessage(),
+//            $e->getTraceAsString()
+//        );
+//
+//        // In an error condition, display an error page.
+//        return array(
+//            'tabOverviewReplacementTemplate' => 'error.tpl',
+//            'templateVariables' => array(
+//                'usefulErrorHelper' => $e->getMessage(),
+//            ),
+//        );
+//    }
+//}
